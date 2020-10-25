689e647de67b91ed208dc7108730f9969ee22d45
devatherock <devatherock@gmail.com>
2020-10-25 07:47:27 -0500

    Introduced test flag for better unit testing

--- src/test/groovy/YamlValidatorSpec.groovy
+++ src/test/groovy/YamlValidatorSpec.groovy
@@ -1,4 +1,10 @@
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.util.logging.Handler
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import java.util.logging.Level
 
 /**
  * Test class to test the groovy script
@@ -6,49 +12,93 @@ import spock.lang.Specification
  * @author z0033zk
  */
 class YamlValidatorSpec extends Specification {
+    Handler handler = Mock()
 
-    def 'test yaml validator'() {
-        setup:
-        def stdout = System.out
-        def stderr = System.err
+    def setup() {
+        Logger.getLogger('').addHandler(handler)
+    }
 
-        and:
-        PrintStream testStdout = Mock()
-        System.setOut(testStdout)
-        System.setErr(testStdout)
-
-        and:
+    @Unroll
+    def 'test yaml validator - valid files, debug: #debugEnabled'() {
+        given:
         StringBuilder outputLogBuilder = new StringBuilder()
-        StringBuilder outputLogBuilderTwo = new StringBuilder()
 
         when: 'debug disabled'
-        YamlValidator.main(['--debug', 'false', '-p',
+        YamlValidator.main(['--debug', "${debugEnabled}", '-p',
                             "${System.properties['user.dir']}/src/test/resources/data/valid"] as String[])
 
         then:
-        (1.._) * testStdout.write(_, _, _) >> { outputLogBuilderTwo.append(new String(it[0])) }
-        String outputLogTwo = outputLogBuilderTwo.toString()
-        outputLogTwo.contains("/config.yml' is valid")
-        outputLogTwo.contains("/multi-doc.yaml' is valid")
-        !outputLogTwo.contains('Validating files in')
-        !outputLogTwo.contains("Validating '")
-        !outputLogTwo.contains('is invalid')
-
-        when: 'debug enabled'
-        YamlValidator.main(['--debug', 'true', '-p',
-                   "${System.properties['user.dir']}/src/test/resources/data/valid"] as String[])
-
-        then:
-        (1.._) * testStdout.write(_, _, _) >> { outputLogBuilder.append(new String(it[0])) }
+        (1.._) * handler.publish(!null as LogRecord) >> { outputLogBuilder.append(it.message) }
         String outputLog = outputLogBuilder.toString()
         outputLog.contains("/config.yml' is valid")
         outputLog.contains("/multi-doc.yaml' is valid")
         outputLog.contains('Validating files in')
         outputLog.contains("Validating '")
         !outputLog.contains('is invalid')
+        levelInvocations * handler.setLevel(Level.FINE)
 
-        cleanup:
-        System.setOut(stdout)
-        System.setErr(stderr)
+        where:
+        debugEnabled || levelInvocations
+        true         || 1
+        false        || 0
+    }
+
+    @Unroll
+    def 'test yaml validator - invalid files, debug: #debugEnabled'() {
+        given:
+        StringBuilder outputLogBuilder = new StringBuilder()
+
+        when: 'debug disabled'
+        YamlValidator.main(['--debug', "${debugEnabled}", '-t', 'true', '-p',
+                            "${System.properties['user.dir']}/src/test/resources/data/invalid"] as String[])
+
+        then:
+        (1.._) * handler.publish(!null as LogRecord) >> { outputLogBuilder.append(it.message) }
+        String outputLog = outputLogBuilder.toString()
+        outputLog.contains("/multi-doc.yml' is invalid")
+        outputLog.contains('Validating files in')
+        outputLog.contains("Validating '")
+        outputLog.contains('is valid')
+        levelInvocations * handler.setLevel(Level.FINE)
+
+        then:
+        RuntimeException exception = thrown()
+        exception.message == 'Invalid yaml files found'
+
+        where:
+        debugEnabled || levelInvocations
+        true         || 1
+        false        || 0
+    }
+
+    @Unroll
+    def 'test yaml validator - invalid files, continueOnError: #continueOnError, debug: #debugEnabled'() {
+        given:
+        StringBuilder outputLogBuilder = new StringBuilder()
+
+        when: 'debug disabled'
+        YamlValidator.main(['--debug', "${debugEnabled}", '-t', 'true', '-c', "${continueOnError}", '-p',
+                            "${System.properties['user.dir']}/src/test/resources"] as String[])
+
+        then:
+        (1.._) * handler.publish(!null as LogRecord) >> { outputLogBuilder.append(it.message) }
+        String outputLog = outputLogBuilder.toString()
+        outputLog.contains(" is invalid")
+        (outputLog.contains("/multi-doc.yml' is invalid") &&
+                outputLog.contains("/anchor.yml' is invalid")) == continueOnError
+        (outputLog.contains("/multi-doc.yaml' is valid") &&
+                outputLog.contains("/config.yml' is valid")) == continueOnError
+        outputLog.contains('Validating files in')
+        outputLog.contains("Validating '")
+        levelInvocations * handler.setLevel(Level.FINE)
+
+        then:
+        RuntimeException exception = thrown()
+        exception.message == 'Invalid yaml files found'
+
+        where:
+        debugEnabled | continueOnError || levelInvocations
+        true         | true            || 1
+        false        | false           || 0
     }
 }
